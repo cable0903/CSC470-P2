2db4b36af8c9ce8d29deae70344ffa57b331266d
Bethany Cable <Bethany.Cable@trojans.dsu.edu>
2020-08-24 21:21:07 -0500

    Created exit button

--- P1 Code/P1 Code/Form1.Designer.cs
+++ P1 Code/P1 Code/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.exit_button = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -36,18 +37,31 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 70F);
             this.label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.label1.Location = new System.Drawing.Point(125, 71);
+            this.label1.Location = new System.Drawing.Point(23, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(529, 107);
             this.label1.TabIndex = 0;
             this.label1.Text = "Hello World";
             // 
+            // exit_button
+            // 
+            this.exit_button.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exit_button.Location = new System.Drawing.Point(369, 171);
+            this.exit_button.Name = "exit_button";
+            this.exit_button.Size = new System.Drawing.Size(149, 50);
+            this.exit_button.TabIndex = 1;
+            this.exit_button.Text = "Exit";
+            this.exit_button.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(574, 355);
+            this.Controls.Add(this.exit_button);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CSC 470 - P1";
@@ -60,6 +74,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button exit_button;
     }
 }
 
